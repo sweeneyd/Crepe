0ae85278307a879715396df260443692f662ee7f
Dan Sweeney <sweeneyd@users.noreply.github.com>
2018-08-18 19:01:39 -0400

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+import Jama.*;
+
+public class Main {
+
+    public static Matrix StiffnessMatrix(){
+
+    }
+
+    public static Matrix GenerateMesh(){
+
+    }
+
+    public static Matrix DOF2Node(int node_number){
+
+
+    }
+
+    public static Matrix Node2DOF(int DOF_number){
+
+    }
+
+    public static Float EvaluatePoint(int DOF_number){
+
+    }
+
+    public static Matrix EvaluatePoint(int DOF_number){
+
+    }
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+    }
+}
